7f474d84bf413089d51db61f9ca08c128cb0ca9f
Samuel John Wylie <sam.wylie20@gmail.com>
2022-01-21 14:20:51 +1300

    Server Changes

--- routes/cities.js
+++ routes/cities.js
@@ -1,17 +1,20 @@
 const express = require('express')
+const db = require('../db/db')
 
 const router = express.Router()
 module.exports = router
 
 // GET City
-router.get('/auckland', (req, res) => {
-  const template = ''
+router.get('/auckland', async (req, res) => {
+  const all = await db.getAll()
 // Make viewData object to be passed back with city data
 const viewData = {
-
+  events: all
 }
 
-res.render(template, viewData)
+console.log(viewData)
+
+res.render('cityLanding', viewData)
 
 })
 router.get('/wellington', (req, res) => {
